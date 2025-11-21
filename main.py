edeae0e0b5942f011ef9dc7db280617758b7ae17
Arif-774729 <ardelight123@gmail.com>
2025-11-21 13:38:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -99,7 +99,7 @@ main_placeholder = st.empty()
 # Gemini LLM
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
-    temperature=0.3,
+    temperature=1,
     google_api_key=GOOGLE_API_KEY
 )
 if process_clicked:
